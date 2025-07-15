f12aca3d43e946a3ec44eb2c8e294943e8a26fcc
vishal anand sadh <vishalanandsadh@gmail.com>
2025-07-15 23:57:31 +0530

    frontend api path set

--- frontend/expense-tracker/src/Utils/apiPath.js
+++ frontend/expense-tracker/src/Utils/apiPath.js
@@ -1,9 +1,19 @@
-import React from 'react'
+const BASE_URL = 'http://localhost:8000';
 
-const apiPath = () => {
-  return (
-    <div>apiPath</div>
-  )
+const API_PATHS = {
+  Auth: {
+    LOGIN: "api/v1/auth/login",
+    REGISTER: "api/v1/auth/register",
+    GET_USER_INFO: "api/v1/auth/getUser",
+  },
+  DASHBOARD:{
+    GET_DATA: "api/v1/dashboard",
+  },
+  INCOME: {
+    ADD_INCOME: "api/v1/income/add",
+    GET_ALL_INCOME: "/api/v1/income/get",
+    DELETE_INCOME: (id) => `api/v1/income/delete/${id}`,
+    DOWNLOAD_EXPENSE: "api/v1/income/downloadexcel"
+  },
+
 }
-
-export default apiPath
