86a42c07af191c843990225ff3acd6b8584fd076
vishalanandsadh13 <vishalanandsadh@gmail.com>
2025-11-07 16:29:48 +0530

    removed console

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,5 @@ app.use("/api/v1/income", incomeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {    
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port no ${PORT}`);
 });
